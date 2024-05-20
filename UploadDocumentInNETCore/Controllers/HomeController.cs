fdbb06e8624e09412bdaad9456811187e194fef3
zahidmustafa@outlook.com <zahidmustafa@outlook.com>
2024-05-20 13:05:21 +0500

    Code

---
+++ UploadDocumentInNETCore/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UploadDocumentInNETCore.Models;
+
+namespace UploadDocumentInNETCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(List<IFormFile> files)
+        {
+            long size = Request.Form.Files.Sum(f => f.Length);
+
+            foreach (var formFile in Request.Form.Files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            return Content("File Uplaoded Sucecssfully");
+        }
+    }
+}
---
+++ UploadDocumentInNETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UploadDocumentInNETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
